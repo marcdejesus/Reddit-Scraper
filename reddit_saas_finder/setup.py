6d4f095d1e70794efb3f54d3834d19a8ca1b043c
Marc De Jesus <marcantoniodejesus@yahoo.com>
2025-07-07 19:50:15 -0400

    Packaging for Distribution

--- reddit_saas_finder/setup.py
+++ reddit_saas_finder/setup.py
@@ -1,17 +1,29 @@
 from setuptools import setup, find_packages
 
+# Read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
-    name="reddit_saas_finder",
-    version="0.1.0",
+    name="reddit-saas-finder",
+    version="1.0.0",
+    author="Marc de Jesus",
+    author_email="marcdejesusk@gmail.com",
+    description="A CLI tool to find SaaS opportunities on Reddit.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marcdejesusk/Reddit-Scraper",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    include_package_data=True,
     entry_points={
         "console_scripts": [
             "reddit-finder=cli.main:app",
         ],
     },
     install_requires=[
-        "typer",
+        "typer[all]",
         "praw",
         "spacy",
         "nltk",
@@ -22,11 +34,12 @@ setup(
         "numpy",
         "rich",
         "PyYAML",
+        "setuptools"
     ],
-    extras_require={
-        "dev": [
-            "pytest",
-            "pytest-cov",
-        ]
-    }
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
 ) 
